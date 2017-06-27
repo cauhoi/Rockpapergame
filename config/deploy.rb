88037ac1d2f76170584db410a825e8a3d241d25a
Damien Le <damien.le@gocanvas.com>
2017-06-27 13:11:59 -0400

    simplify deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,24 +27,24 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 
 set :passenger_restart_command, 'passenger-config restart-app'
 
-namespace :deploy do
-desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
+# namespace :deploy do
+# desc 'Restart application'
+#   task :restart do
+#     on roles(:app), in: :sequence, wait: 5 do
+#       execute :touch, release_path.join('tmp/restart.txt')
+#     end
+#   end
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
+#   after :restart, :clear_cache do
+#     on roles(:web), in: :groups, limit: 3, wait: 10 do
+#       # Here we can do anything such as:
+#       # within release_path do
+#       #   execute :rake, 'cache:clear'
+#       # end
+#     end
+#   end
 
-end
+# end
 # set :rvm_ruby_version, '2.1.0'
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", "config/secrets.yml"
