831a5228ce30567e13ae7b83c60909e8ec8cfbda
Damien Le <damien.le@gocanvas.com>
2017-06-27 23:33:40 -0400

    new production server C

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,8 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '54.244.154.20', user: 'deploy', roles: %w{web app db}
-
+server '34.211.145.147', user: 'deploy', roles: %w{web app db}
 
 # role-based syntax
 # ==================
